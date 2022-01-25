79773598f0645b1945cef1bbc10eeda0bc62dc64
Sirinoks <shinigamoo@gmail.com>
2022-01-25 06:50:57 -0500

    basic structure and leaderboard

---
+++ kotgh/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+  <div class="panels">
+    <div class="col left-panel">
+      <Leaderboard />
+    </div>
+    <div class="col center-panel"></div>
+    <div class="col right-panel"></div>
+  </div>
+</template>
+
+<script>
+import Leaderboard from './components/Leaderboard.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Leaderboard
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+
+.panels {
+  display: flex;
+  flex-direction: row;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.col {
+  display: flex;
+  width: 30%;
+  height: 100vh;
+}
+
+.left-panel {
+  background-color: #f3aaaa;
+  flex-direction: column;
+
+}
+
+.center-panel {
+  background-color: #8ed66d;
+}
+
+.right-panel {
+  background-color: #5d94c7;
+}
+</style>
---
+++ kotgh/src/components/Leaderboard.vue
@@ -0,0 +1,53 @@
+<template>
+    <div id="ldbrd">
+        <leader-user v-for="u in users" :key="u.name">
+            <div class="user">
+                <span class="uname">{{ u.name }}</span>
+                <img class="leaderb-portrait" :src="u.img" />
+                <button>{{ u.class }}</button>
+            </div>
+        </leader-user>
+    </div>
+</template>
+<script>
+import { defineComponent } from "vue"
+
+
+export default defineComponent({
+    setup() {
+        return {
+            users: [
+                { name: "Bratishka", img: "https://cdn.discordapp.com/attachments/353432193730871296/935479115740504134/bratishka.png", class: "s" },
+                { name: "Tolya", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107390935100/tolya.png", class: "i" },
+                { name: "LeCrisp", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107747483668/le_crisp.png", class: "x" },
+                { name: "AWDKA", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108036878397/awdka.png", class: "x" },
+                { name: "Octopus", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108410159164/octopus.png", class: "g" },
+                { name: "Gleb", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108653441024/gleb.png", class: "x" }
+            ]
+        }
+    }
+})
+
+</script>
+<style>
+.user {
+    flex-direction: row;
+    display: flex;
+    background-color: rgba(100, 100, 100, 0.5);
+}
+
+.uname {
+    flex-grow: 4;
+    text-align: right;
+}
+
+.leaderb-portrait {
+    width: 3vh;
+}
+
+button {
+    border: solid black 1px;
+    margin: 0;
+    padding: 0 0.3vh;
+}
+</style>
