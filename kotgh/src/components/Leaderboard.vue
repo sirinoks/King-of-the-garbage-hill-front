0a9b8592a8f6c25613d143cf6547b7d7320ddf04
Sirinoks <shinigamoo@gmail.com>
2022-01-29 08:36:20 -0500

    click function

--- kotgh/src/components/Leaderboard.vue
+++ kotgh/src/components/Leaderboard.vue
@@ -3,30 +3,63 @@
         <leader-user v-for="u in users" :key="u.name">
             <div class="user">
                 <span class="uname">{{ u.name }}</span>
-                <img class="leaderb-portrait" :src="u.img" />
                 <button>{{ u.class }}</button>
+                <img
+                    @click="hidden = !hidden; nam=u.name"
+                    class="leaderb-portrait"
+                    :src="u.img"
+                    v-if="!hidden"
+                />
+                <div class="charsSelect" v-else-if="nam==u.name">
+                    <img v-for="c in chars" :key="c.name" :src="c.img" />
+                </div>
             </div>
         </leader-user>
     </div>
 </template>
+
+
 <script>
-import { defineComponent } from "vue"
+import { defineComponent, ref } from "vue"
 
 
 export default defineComponent({
     setup() {
+        const hidden = ref(false);
+        const nam = ref("x");
+
         return {
             users: [
-                { name: "Bratishka", img: "https://cdn.discordapp.com/attachments/353432193730871296/935479115740504134/bratishka.png", class: "s" },
-                { name: "Tolya", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107390935100/tolya.png", class: "i" },
-                { name: "LeCrisp", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107747483668/le_crisp.png", class: "x" },
+                { name: "Excellify", img: "https://cdn.discordapp.com/attachments/353432193730871296/935479115740504134/bratishka.png", class: "s" },
+                { name: "AnatoleAM", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107390935100/tolya.png", class: "i" },
+                { name: "EncycloAcid", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107747483668/le_crisp.png", class: "x" },
                 { name: "AWDKA", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108036878397/awdka.png", class: "x" },
                 { name: "Octopus", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108410159164/octopus.png", class: "g" },
                 { name: "Gleb", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108653441024/gleb.png", class: "x" }
-            ]
+            ],
+            chars: [
+                { name: "Bratishka", img: "https://cdn.discordapp.com/attachments/353432193730871296/935479115740504134/bratishka.png" },
+                { name: "Tolye", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107390935100/tolya.png" },
+                { name: "LeCrisp", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494107747483668/le_crisp.png" },
+                { name: "AWDKA", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108036878397/awdka.png" },
+                { name: "Octopus", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108410159164/octopus.png" },
+                { name: "Gleb", img: "https://cdn.discordapp.com/attachments/353432193730871296/935494108653441024/gleb.png" }
+            ],
+            hidden,
+            nam
         }
+       
+
     }
 })
+
+
+/*
+const openChars = new Vue({
+    el: "#user",
+
+})
+*/
 
 </script>
 <style>
@@ -49,5 +82,17 @@ button {
     border: solid black 1px;
     margin: 0;
     padding: 0 0.3vh;
+}
+
+.hidden {
+    display: none;
+}
+
+.charsSelect {
+    flex-direction: column;
+}
+
+.charsSelect img {
+    width: 3vh;
 }
 </style>
