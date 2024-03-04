022015dc545749f04e518ab5b27dbeb8ca43a49d
Sirinoks <shinigamoo@gmail.com>
2024-03-04 00:53:25 -0500

    Home page components added

--- kotgh/src/App.vue
+++ kotgh/src/App.vue
@@ -1,45 +1,25 @@
 <template>
-  <div class="logins">
-    <LoginProcess v-if="!isLoggedIn" @loginClick="setLogin" version="1.0"></LoginProcess>
-    <LoginSuccess v-else version="1.0" :username="username"></LoginSuccess>
+  <div class="app">
+    <RouterView />
   </div>
-
-  <div>
-    <!-- <PatchNotes></PatchNotes> -->
-  </div>
-  <RouterView />
 </template>
 
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import LoginProcess from '@/views/Login/LoginProcess.vue'
-import LoginSuccess from '@/views/Login/LoginSuccess.vue'
+import LoginPage from '@/views/Login/LoginPage.vue'
+
 import PatchNotes from '@/components/Home/PatchNotes.vue'
 import { ref } from 'vue'
 
 import { PhHorse, PhHeart, PhCube } from '@phosphor-icons/vue'
 
-const isLoggedIn = ref(false)
-const username = ref('Sirinoks')
-
-const setLogin = () => {
-  isLoggedIn.value = true
-}
 </script>
 
 <style scoped>
 * {
   display: flex;
 }
-.logins {
-  width: 100vw;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  padding-bottom: 20vh;
-}
-.logins > div {
-  justify-content: center;
+.app{
+  flex-direction: column;
 }
 </style>
--- kotgh/src/router/index.ts
+++ kotgh/src/router/index.ts
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import HomePage from '../views/Home/HomePage.vue'
+import HomePage from '@/views/Home/HomePage.vue'
+import LoginPage from '@/views/Login/LoginPage.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,7 +8,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'default',
-      component: HomeView
+      component: LoginPage,
     },
     {
       path: '/about',
@@ -21,9 +21,6 @@ const router = createRouter({
     {
       path: '/home',
       name: 'HomePage',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: HomePage
     }
   ]
