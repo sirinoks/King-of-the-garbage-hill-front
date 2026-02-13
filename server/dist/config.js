5e51a2378d88ca5bce8d4d5e39930f5a528f1466
Sirinoks <shinigamoo@gmail.com>
2026-02-13 16:27:06 -0500

    Clean up dist

--- server/dist/config.js
+++
@@ -1,4 +0,0 @@
-// server/src/config.ts
-import dotenv from 'dotenv';
-dotenv.config();
-export const PORT = process.env.PORT || 53134;
--- server/dist/utils/html.d.ts
+++
@@ -1,2 +0,0 @@
-import type { ViteDevServer } from 'vite';
-export declare function renderHtml(appHtml: string, css: string, server: ViteDevServer): Promise<string>;
